ee9e878d482347f73ade029bc9a17c2d532a5a83
guillaume-boop <150714048+guillaume-boop@users.noreply.github.com>
2024-03-05 23:01:05 +0100

    Update degrade.js

--- degrade.js
+++ degrade.js
@@ -5,8 +5,9 @@ var granimInstance = new Granim({
     states : {
         "default-state": {
             gradients: [
-               ['#FFFF00', '#FFD700', '#FFA500'], // Dégradé avec le jaune comme couleur principale
-               ['#FF69B4', '#FF1493', '#FF00FF'] 
+                ['#FF69B4', '#FF1493', '#FF00FF'], // Rose
+                ['#FFFF00', '#FFD700', '#FFA500'], // Jaune
+                ['#00FF00', '#ADFF2F', '#7FFF00']  // Vert clair
             ],
             transitionSpeed: 2000 // Vitesse de transition en millisecondes
         }
