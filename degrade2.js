d506fc0ac2ba43d496777e84bd1df6a368ffdbfa
guillaume-boop <150714048+guillaume-boop@users.noreply.github.com>
2024-03-06 17:29:07 +0100

    Update degrade2.js

--- degrade2.js
+++ degrade2.js
@@ -5,7 +5,7 @@ var granimInstance = new Granim({
     states : {
         "default-state": {
             gradients: [
-                ['#833ab4', '#fd1d1d', '#fcb045'], // Premier dégradé de couleurs
+                ['#833ab4', '#fd1d1d', '#fcb045']// Premier dégradé de couleurs
 
             ],
 
