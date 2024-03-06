56d812494895d1650e79fab005a9893039f3b48f
guillaume-boop <150714048+guillaume-boop@users.noreply.github.com>
2024-03-06 17:20:26 +0100

    Update degrade.js

--- degrade.js
+++ degrade.js
@@ -1,17 +1,4 @@
-var granimInstance = new Granim({
-    element: '#granim-canvas',
-    direction: 'left-right',
-    isPausedWhenNotInView: true,
-    states : {
-        "default-state": {
-            gradients: [
-                ['#FFFF00', '#FFD700', '#FFA500'], // Dégradé avec le jaune comme couleur principale
-                ['#FF69B4', '#FF1493', '#FF00FF']
-            ],
-            transitionSpeed: 2000 // Vitesse de transition en millisecondes
-        }
-    }
-});
+
   function retournerCarte() {
   const carte = document.querySelector('.carte');
   carte.classList.toggle('retournee');
@@ -52,3 +39,19 @@ function retournerCarte8() {
   const carte = document.querySelector('.carte8');
   carte.classList.toggle('retournee8');
   }
+
+
+var granimInstance = new Granim({
+    element: '#granim-canvas',
+    direction: 'left-right',
+    isPausedWhenNotInView: true,
+    states : {
+        "default-state": {
+            gradients: [
+                ['#FFFF00', '#FFD700', '#FFA500'], // Dégradé avec le jaune comme couleur principale
+                ['#FF69B4', '#FF1493', '#FF00FF']
+            ],
+            transitionSpeed: 2000 // Vitesse de transition en millisecondes
+        }
+    }
+});
