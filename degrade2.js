2b6e982062f4c28abe1848d0f82f3b5acbc4a0e2
guillaume-boop <150714048+guillaume-boop@users.noreply.github.com>
2024-03-06 18:45:21 +0100

    Update degrade2.js

--- degrade2.js
+++ degrade2.js
@@ -5,14 +5,11 @@ var granimInstance = new Granim({
     states : {
         "default-state": {
             gradients: [
-                ['#1A1A2E', '#5D5E70', '#9D9EAB'], // Dégradé de bleu foncé à violet à gris foncé
-                ['#1E212D', '#4E5166', '#A9A9A9'], // Dégradé de bleu foncé à violet à gris foncé
-                ['#252628', '#58595B', '#9C9D9E']  // Dégradé de bleu foncé à violet à gris foncé
+                ['#1A1A2E', '#008080', '#9D9EAB'], // Dégradé de bleu foncé à bleu canard foncé à gris foncé
+                ['#1E212D', '#004953', '#A9A9A9'], // Dégradé de bleu foncé à bleu canard foncé à gris foncé
+                ['#252628', '#003B46', '#9C9D9E']  // Dégradé de bleu foncé à bleu canard foncé à gris foncé
             ],
             transitionSpeed: 2000 // Vitesse de transition en millisecondes
         }
     }
 });
-
-
-
